ee1521215f47c8e05636726fe32f44f7e4279050
rachelbookhout <rbookhou@gmail.com>
2014-10-04 13:10:10 -0400

    Car new and index is working

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,6 @@ class Car < ActiveRecord::Base
    validates :manufacturer_id, presence:true
    validates :color, presence:true
    validates :year, presence:true # needs to be above 1920
-   validates_numericality_of :year, greater_than 1920
+   validates :year, :numericality => {:greater_than => 1920}
    validates :mileage, presence:true
-   validates :description
 end
